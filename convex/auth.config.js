503a3d8a89ac304334a845759ac650934df742ab
Arpan <arpankr06@gmail.com>
2024-03-06 04:40:17 +0530

    first commit

---
+++ convex/auth.config.js
@@ -0,0 +1,9 @@
+export default {
+    providers:[
+        {
+            // domain:"https://exotic-monitor-65.clerk.accounts.dev",
+            domain:"https://humble-ewe-14.clerk.accounts.dev",
+            applicationID: "convex",
+        }
+    ]
+}
